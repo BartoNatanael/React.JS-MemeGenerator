573e744d45cf315c16b2c9355a4d551fe786c4b5
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-04-23 16:10:36 +0200

    masonry grid added

--- src/components/imageChooser.js
+++ src/components/imageChooser.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Masonry from 'react-masonry-css';
 
-
+const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
 
 const MemeChoser = (props) => {
     let number = 0
@@ -8,9 +14,16 @@ const MemeChoser = (props) => {
     return ( 
         <div className="MemeChooser">
             <h1 className="MemeChooser__title">Choose your image</h1>
-            <div className="MemeChooser__memes">
+            {/* <div className="MemeChooser__memes"> */}
+                
+            {/* </div> */}
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="MemeChooser__memes"
+                columnClassName="MemeChooser__column"
+                >
                 {memes}
-            </div>
+            </Masonry>
         </div>
      );
 }
