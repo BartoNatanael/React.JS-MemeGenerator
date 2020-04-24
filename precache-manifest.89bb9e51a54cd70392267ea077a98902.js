9e83fdb912ed7fc80a3f25957c4a4f95b4382a93
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-04-24 11:35:09 +0200

    Updates

---
+++ precache-manifest.89bb9e51a54cd70392267ea077a98902.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acdf71c3934535666b8e04404881396e",
+    "url": "/React.JS-MemeGenerator/index.html"
+  },
+  {
+    "revision": "2a8415086fa9a6cb0fc9",
+    "url": "/React.JS-MemeGenerator/static/css/main.212670d5.chunk.css"
+  },
+  {
+    "revision": "a34520c4d73c17b5d4e1",
+    "url": "/React.JS-MemeGenerator/static/js/2.f6c6b61f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React.JS-MemeGenerator/static/js/2.f6c6b61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8415086fa9a6cb0fc9",
+    "url": "/React.JS-MemeGenerator/static/js/main.ba50c437.chunk.js"
+  },
+  {
+    "revision": "aa9285d57bc362b1aa98",
+    "url": "/React.JS-MemeGenerator/static/js/runtime-main.594f7231.js"
+  },
+  {
+    "revision": "f33ac0e891c239bc1ff662de5875a7db",
+    "url": "/React.JS-MemeGenerator/static/media/blackkid.f33ac0e8.jpg"
+  },
+  {
+    "revision": "82f084d53d500b11485651e751f99cce",
+    "url": "/React.JS-MemeGenerator/static/media/bob.82f084d5.jpg"
+  },
+  {
+    "revision": "e9c674416f5672568c916acd15b70cf1",
+    "url": "/React.JS-MemeGenerator/static/media/butter.e9c67441.jpg"
+  },
+  {
+    "revision": "0796c4d6e79b677939ecb1ba1b37e2e5",
+    "url": "/React.JS-MemeGenerator/static/media/cat.0796c4d6.jpg"
+  },
+  {
+    "revision": "69d8c4f1a7f51c6f9728d1676058e851",
+    "url": "/React.JS-MemeGenerator/static/media/death.69d8c4f1.jpg"
+  },
+  {
+    "revision": "04476b01b74c427abaa622949b64de98",
+    "url": "/React.JS-MemeGenerator/static/media/funnykid.04476b01.jpg"
+  },
+  {
+    "revision": "db341d750ee3ffd2f74b147b51caa231",
+    "url": "/React.JS-MemeGenerator/static/media/guys.db341d75.png"
+  },
+  {
+    "revision": "881d35c4a7c058a544e6498d524016e1",
+    "url": "/React.JS-MemeGenerator/static/media/kid.881d35c4.jpg"
+  },
+  {
+    "revision": "76ace3bb5acc44aa62df75e4ee8dee0d",
+    "url": "/React.JS-MemeGenerator/static/media/killerdoge.76ace3bb.jpg"
+  },
+  {
+    "revision": "778d2a6d9a74b20e3e86496d94a4d3bc",
+    "url": "/React.JS-MemeGenerator/static/media/logo.778d2a6d.png"
+  },
+  {
+    "revision": "b75db761531f5f851c1ccb2c21712934",
+    "url": "/React.JS-MemeGenerator/static/media/money.b75db761.png"
+  },
+  {
+    "revision": "12406578a5fbe50a019339138110976c",
+    "url": "/React.JS-MemeGenerator/static/media/noszacz.12406578.jpg"
+  },
+  {
+    "revision": "8428a98c06fba0cafc52f59fc0004c45",
+    "url": "/React.JS-MemeGenerator/static/media/papiez.8428a98c.jpg"
+  },
+  {
+    "revision": "cb1ff076911d3b44611f9d1bbf3be728",
+    "url": "/React.JS-MemeGenerator/static/media/pikachu.cb1ff076.jpg"
+  },
+  {
+    "revision": "36a4984180b36c0a48372626a777dbce",
+    "url": "/React.JS-MemeGenerator/static/media/rex.36a49841.jpg"
+  },
+  {
+    "revision": "1bcc4eabafc46ffe9330584d5c23d367",
+    "url": "/React.JS-MemeGenerator/static/media/smilebob.1bcc4eab.jpg"
+  },
+  {
+    "revision": "04db8676979e9604ac16368c63ae5ab4",
+    "url": "/React.JS-MemeGenerator/static/media/tablecat.04db8676.png"
+  },
+  {
+    "revision": "e0b35ef9a367fdfcfa31247b0f0e6ae2",
+    "url": "/React.JS-MemeGenerator/static/media/whatif.e0b35ef9.jpg"
+  }
+]);
